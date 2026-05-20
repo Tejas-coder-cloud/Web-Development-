a55dd908aae928d4524eafa05675660822b06322
Tejas07 <dhatraktejas07@gmail.com>
2026-05-20 15:02:05 +0530

    Added the code for react hook

--- React/New folder/Hook/React-hook/src/App.jsx
+++ React/New folder/Hook/React-hook/src/App.jsx
@@ -4,12 +4,49 @@ import {useForm} from "react-hook-form"
 
 function App() {
   const [count, setCount] = useState(0)
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, watch, formState: { errors,isSubmitting } } = useForm();
+  async function onSubmit(data)
+  {
+    await new Promise((resolve)=> setTimeout(resolve,5000));
+    console.log("Form submitted",data);
+    
+  }
 
   return (
-    <div>
-     Hello Jee
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+      
+        <label >
+         First Name:  </label>
+        <input type="text" {...register('Firstname', { 
+          required: true, maxLength: 20 ,minLength: {value:4,message:"Minimum 4 letters required"}})} />
+          {errors.Firstname && <p className='errorclass'> {errors.Firstname.message}</p>}
+      </div>
+      <div>
+        <br/>
+        <label >
+         Middle Name:  </label>
+        <input type="text" {...register('Middlename')} />
+      </div>
+      <br/>
+         <div>
+        <label >
+         Last Name:  </label>
+        <input type="text"{...register('Lastname', { 
+          pattern:{
+            value: /^[A-Za-z]+$/i,
+            message:"Last name is not as per regex"
+          }
+           })} />
+           {errors.Lastname && <p className='errorclass'>{errors.Lastname.message}</p>}
+      </div>
+      <br />
+      <input type="submit" disabled={isSubmitting} 
+       value={isSubmitting ?"Submitted" : "Submit"}/>
+    </form>
+    // <div>
+    //  Hello Jee
+    // </div>
   )
 }
 
